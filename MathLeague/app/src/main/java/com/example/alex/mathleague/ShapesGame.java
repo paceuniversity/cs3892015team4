043fb95710b19f2bdb6e36fad45f2ef3f7defb57
OrtizAlex <ao16030n@pace.edu>
2015-04-14 11:43:48 -0400

    second version

--- MathLeague/app/src/main/java/com/example/alex/mathleague/ShapesGame.java
+++ MathLeague/app/src/main/java/com/example/alex/mathleague/ShapesGame.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
 
 public class ShapesGame extends ActionBarActivity {
@@ -11,6 +13,7 @@ public class ShapesGame extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_shapes_game);
     }
 
