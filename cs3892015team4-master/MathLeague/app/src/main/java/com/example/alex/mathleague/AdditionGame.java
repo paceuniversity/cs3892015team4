2988a4cf8aae0da91b4c7da918f492f28327dee6
TiarahNS <ts33337p@pace.edu>
2015-04-17 17:47:36 -0400

    TS version 2
    Merge-able version of my code

--- cs3892015team4-master/MathLeague/app/src/main/java/com/example/alex/mathleague/AdditionGame.java
+++ cs3892015team4-master/MathLeague/app/src/main/java/com/example/alex/mathleague/AdditionGame.java
@@ -1,24 +1,28 @@
 package com.example.alex.mathleague;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
 
-public class SubtractionGame extends ActionBarActivity {
+public class AdditionGame extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_subtraction_game);
+
+        setContentView(R.layout.activity_addition_game);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_subtraction_game, menu);
+        getMenuInflater().inflate(R.menu.menu_addition_game, menu);
         return true;
     }
 
--- cs3892015team4-master/MathLeague/app/src/main/java/com/example/alex/mathleague/SubtractionGame.java
+++ cs3892015team4-master/MathLeague/app/src/main/java/com/example/alex/mathleague/SubtractionGame.java
@@ -1,24 +1,28 @@
 package com.example.alex.mathleague;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
 
-public class FourthActivity extends ActionBarActivity {
+public class SubtractionGame extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fourth);
+
+        setContentView(R.layout.activity_subtraction_game);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_fourth, menu);
+        getMenuInflater().inflate(R.menu.menu_subtraction_game, menu);
         return true;
     }
 
