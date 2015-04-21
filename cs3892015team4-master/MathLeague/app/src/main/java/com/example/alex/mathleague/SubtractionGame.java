30bc758d3ddcd2c1f6e9f1074459edc8ffbd8e04
TiarahNS <ts33337p@pace.edu>
2015-04-21 09:52:52 -0400

    TS Version 3
    Subtraction game added

--- cs3892015team4-master/MathLeague/app/src/main/java/com/example/alex/mathleague/SubtractionGame.java
+++ cs3892015team4-master/MathLeague/app/src/main/java/com/example/alex/mathleague/SubtractionGame.java
@@ -5,19 +5,70 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.*;
 
 
 public class SubtractionGame extends Activity {
+
+    public TextView ans1B, ans2B, ans3B, ans4B;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_subtraction_game);
-    }
 
+
+         ans1B = (TextView) this.findViewById(R.id.ans1);
+         ans2B = (TextView) this.findViewById(R.id.ans2);
+         ans3B = (TextView) this.findViewById(R.id.ans3);
+         ans4B = (TextView) this.findViewById(R.id.ans4);
+
+
+
+        ans1B.setOnClickListener(new OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Correct!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ans2B.setOnClickListener(new OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Sorry, try again", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ans3B.setOnClickListener(new OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Sorry, try again", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ans4B.setOnClickListener(new OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Sorry, try again", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -40,4 +91,6 @@ public class SubtractionGame extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
